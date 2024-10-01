4ee5219cdf303f2be2cb78a71852ee58a2f1470e
iscogonzalez18 <83782270+iscogonzalez18@users.noreply.github.com>
2024-10-01 16:17:00 +0200

    ej 3 done

--- IntroducciónMatlab/Matlab(1)/Ejercicio3.m
+++ IntroducciónMatlab/Matlab(1)/Ejercicio3.m
@@ -12,4 +12,33 @@ else
     cols = dimensiones(2)
 end
 
-Matriz = rellenarMatriz([filas cols])
+fprintf("2 y 3\n")
+A = rellenarMatriz([filas cols]);
+B = rellenarMatriz([filas cols]);
+
+fprintf("4\n")
+A
+B
+
+A_transpuesta = B'
+B_transpuesta = B'
+
+A_inversa = inv(A);
+B_inversa = inv(B);
+
+det_A = det(A);
+det_B = det(B);
+
+rango_A = rank(A)
+rango_B = rank(B)
+
+producto_matricial = A * B
+
+producto_elemento_a_elemento = A .* B
+
+vector_fila = [A(1, :) B(1, :)];
+
+vector_columna = [A(:, 1); B(:, 1)];
+
+
+
--- IntroducciónMatlab/Matlab(1)/rellenarMatriz.m
+++ IntroducciónMatlab/Matlab(1)/rellenarMatriz.m
@@ -1,3 +1,22 @@
 function Matriz = rellenarMatriz(dim)
-    Matriz = randi([1, 10], dim[1], dim[2]);
+    Matriz = zeros(dim(1), dim(2));
+
+    for i = 1:dim(1)
+        for j = 1:dim(2)
+            entrada = input(['Ingrese un valor para la posición (', num2str(i), ',', num2str(j), ') o "r" para rellenar el resto aleatoriamente: '], 's'); % string
+            
+            if entrada == 'r'
+                for ii = i:dim(1)
+                    for jj = (j:dim(2))
+                        Matriz(ii, jj) = randi([1, 10]);
+                    end
+                    j = 1; % recogemos carrete para nueva fila
+                end
+                return; % Salir de la función ya que la matriz está completa
+            else
+                % Convertir la entrada a un número y asignarlo a la matriz
+                Matriz(i, j) = str2double(entrada);
+            end
+        end
+    end
 end
