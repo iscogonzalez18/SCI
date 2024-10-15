57013ee9070d34ba6415981a4cb8907aba9aa18c
iscogonzalez18 <83782270+iscogonzalez18@users.noreply.github.com>
2024-10-15 17:11:26 +0200

    stop simulation problem fixed

--- 1-IdentificaciónYControlNeural/Parte2/RunPositionControl.m
+++ 1-IdentificaciónYControlNeural/Parte2/RunPositionControl.m
@@ -1,15 +1,36 @@
 %Tiempo de muestreo
-% Ts=100e-3 
-% % Referencia x-y de posicion
-% refx=2.0; 
-% refy=2.0;
-% % Ejecutar Simulacion
-% sim('PositionControl.slx')
+Ts=100e-3 
+% Referencia x-y de posicion
+refx=2.0; 
+refy=2.0;
+% Ejecutar Simulacion
+sim('PositionControl.slx')
+
+% Generar N posiciones aleatorias, simular y guardar en variables
+% N=30
+% E_d_vec=[];
+% E_theta_vec=[];
+% V_vec=[];
+% W_vec=[];
+% for i=1:N
+%     refx=10*rand-5;
+%     refy=10*rand-5;
+%     sim('PositionControl.slx')
+%     E_d_vec=[E_d_vec;E_d.signals.values];
+%     E_theta_vec=[E_theta_vec;E_theta.signals.values];
+%     V_vec=[V_vec; V.signals.values];
+%     W_vec=[W_vec; W.signals.values];
+% end
+% inputs=[E_d_vec'; E_theta_vec'];
+% outputs=[V_vec'; W_vec'];
 
 % Mostrar
 x=out_x.signals.values;
 y=out_y.signals.values;
 figure;
 plot(x,y);
+title('Representación de Movimiento');
+xlabel('Eje X');
+ylabel('Eje Y');
 grid on;
-hold on; 
+hold on;
