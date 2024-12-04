66eb0e1bbdbfc72c5ab13a897472d6128f5aae88
iscogonzalez18 <83782270+iscogonzalez18@users.noreply.github.com>
2024-12-04 15:46:43 +0100

    se pasa de frenada pero va de locos

--- 2-ControlBorroso/Parte 1/TryOut.m
+++ 2-ControlBorroso/Parte 1/TryOut.m
@@ -1,8 +1,8 @@
 %Tiempo de muestreo
 Ts=100e-3 
 % Referencia x-y de posicion
-refx=10*rand-5 
-refy=10*rand-5
+refx=10*rand-5;
+refy=10*rand-5;
 % Ejecutar Simulacion
 sim('PositionControl.slx')
 % Mostrar
